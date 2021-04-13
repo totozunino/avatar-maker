1dbc04d0990db26c3ef84a2cb39138f8df6da1c6
Mathias Zunino <mathiaszunino@gmail.com>
2021-04-13 02:02:12 -0300

    feat: navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="nav">
+    <img src="@/assets/images/logo.png" alt="Logo" class="nav__logo" @click="goToWebsite" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component({})
+export default class Navbar extends Vue {
+  private goToWebsite() {
+    window.location.href = "https://mzunino.com.uy";
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.nav {
+  padding: 1.4rem 0.5rem 0px;
+  margin: 0 5% !important;
+  display: flex;
+  justify-content: space-between;
+
+  &__logo {
+    width: 78px;
+  }
+}
+</style>
